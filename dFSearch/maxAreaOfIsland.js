23aa778141c2f54816a747b5dcfa088460b6931e
PeterCrosta <pscrosta@gmail.com>
2021-01-04 15:51:17 -0800

    max area of island

---
+++ dFSearch/maxAreaOfIsland.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+    let max = 0;
+    let curr = 0;
+    let set = new Set();
+    const counter = (row, col) => {
+        if (set.has(`${row} x ${col}`) || !grid[row] || !grid[row][col]) return curr
+        set.add(`${row} x ${col}`)
+        curr++
+        let up = counter(row+1,col) 
+        let down = counter(row-1,col)
+        let left = counter(row,col-1)
+        let right = counter(row,col+1)
+        return Math.max(up,down,left,right)
+        
+    }
+    for (let i = 0; i < grid.length; i++) {
+        let row = grid[i];
+        for (let j = 0; j < row.length; j++) {
+            let sq = row[j];
+            let text = `${i} x ${j}`
+            if (!sq || set.has(text)) continue;
+            let count = counter(i,j);
+            if (count > max) max = count;
+            curr = 0;
+        }
+    }
+    return max
+};
