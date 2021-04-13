59abb9d1df92ec3bfb0802821916cf960cd9e23e
PeterCrosta <pscrosta@gmail.com>
2021-04-13 12:03:12 -0700

    BFS 31 percent

--- bst/deepest-leaves-sum.js
+++ bst/deepest-leaves-sum.js
@@ -10,3 +10,19 @@ var deepestLeavesSum = function(root) {
     search()
     return sums[sums.length-1]
 };
+
+var deepestLeavesSum = function(root) {
+    let sum = 0, q = [root];
+    while (q.length) {
+        const next = []
+        let curr = 0
+        for (let node of q) {
+            curr+=node.val
+            if (node.left) next.push(node.left)
+            if (node.right) next.push(node.right)
+        }
+        sum = curr
+        q = next
+    }
+    return sum
+};
