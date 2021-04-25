6e3e2ad1205226381e869bf03a01b51d0a31c8d3
PeterCrosta <pscrosta@gmail.com>
2021-04-25 10:17:35 -0700

    solved iteratively, 82 percent

--- bst/invert-binary-tree.js
+++ bst/invert-binary-tree.js
@@ -1,13 +1,27 @@
+// var invertTree = function(root) {
+//     if (!root) return root
+//     const travers = (node = root) => {
+//         if (!node) return
+//         const rHold = node.right
+//         node.right = node.left
+//         node.left = rHold
+//         travers(node.left)
+//         travers(node.right)
+//     }
+//     travers()
+//     return root
+// };
+
 var invertTree = function(root) {
     if (!root) return root
-    const travers = (node = root) => {
-        if (!node) return
+    const stack = [root]
+    while (stack.length) {
+        const node = stack.pop()
+        if (node.left) stack.push(node.left)
+        if (node.right) stack.push(node.right)
         const rHold = node.right
         node.right = node.left
         node.left = rHold
-        travers(node.left)
-        travers(node.right)
     }
-    travers()
     return root
 };
