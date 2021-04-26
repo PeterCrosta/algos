eaeb94f3a7f5a7529c24bf1e8167e573d68c83cf
PeterCrosta <pscrosta@gmail.com>
2021-04-26 10:27:33 -0700

    queue solution

--- classes/design-hit-counter.js
+++ classes/design-hit-counter.js
@@ -1,19 +1,39 @@
-var HitCounter = function() {
-    this.hits = []
-};
+// var HitCounter = function() {
+//     this.hits = []
+// };
 
+
+// HitCounter.prototype.hit = function(timestamp) {
+//     this.hits[timestamp] = this.hits[timestamp] || 0
+//     this.hits[timestamp]++
+// };
+
+
+// HitCounter.prototype.getHits = function(timestamp) {
+//     let total = 0
+//     for (let i = timestamp - 299 >= 0 ? timestamp - 299 : 0; i <= timestamp; i++) {
+//         if (!this.hits[i]) continue
+//         total += this.hits[i]
+//     }
+//     return total
+// };
+
+var HitCounter = function() {
+    this.hits = [[0,0]]
+};
 
 HitCounter.prototype.hit = function(timestamp) {
-    this.hits[timestamp] = this.hits[timestamp] || 0
-    this.hits[timestamp]++
+    const {hits} = this, last = hits[hits.length - 1]
+    if (last[0] === timestamp) last[1]++
+    else hits.push([timestamp,1])
 };
 
-
 HitCounter.prototype.getHits = function(timestamp) {
-    let total = 0
-    for (let i = timestamp - 299 >= 0 ? timestamp - 299 : 0; i <= timestamp; i++) {
-        if (!this.hits[i]) continue
-        total += this.hits[i]
+    const {hits} = this
+    let i = hits.length - 1, count = 0
+    while (i >= 0 && hits[i][0] > timestamp - 300) {
+        count += hits[i][1]
+        i--
     }
-    return total
+    return count
 };
